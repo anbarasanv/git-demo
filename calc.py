34e3dc124ada5fc3c1723f8bed08ed80e8d8097a
Anbarasan <anbarasan.mailme@gmail.com>
2019-08-25 20:46:05 +0530

    Multiplication function has been modified

--- calc.py
+++ calc.py
@@ -1,12 +1,22 @@
+import functools as ft
 def add(*args):
-	"""Adding list of values"""
-	return sum(args)
-def mul(x,y):
-	return x*y
-def div(x,y):
-	return x//y
-def sub(x,y):
-	return x-y
+    """Adding list of values"""
+    return sum(args)
 
-print(add(1,2,4))
-print(sub(75,90))
+
+def mul(*args):
+    x = lambda a, b: a*b
+    return ft.reduce(x, args)
+
+
+def div(x, y):
+    return x // y
+
+
+def sub(x, y):
+    return x - y
+
+
+print(add(1, 2, 4))
+print(sub(75, 90))
+print(mul(1, 24, 2))
