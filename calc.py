c0bcd340d48857c96130ba6c626792ade6178b8d
Anbarasan <anbarasan.mailme@gmail.com>
2019-08-25 20:50:09 +0530

    Revert "Multiplication function has been modified"
    This reverts commit 34e3dc124ada5fc3c1723f8bed08ed80e8d8097a.

--- calc.py
+++ calc.py
@@ -1,22 +1,12 @@
-import functools as ft
 def add(*args):
-    """Adding list of values"""
-    return sum(args)
+	"""Adding list of values"""
+	return sum(args)
+def mul(x,y):
+	return x*y
+def div(x,y):
+	return x//y
+def sub(x,y):
+	return x-y
 
-
-def mul(*args):
-    x = lambda a, b: a*b
-    return ft.reduce(x, args)
-
-
-def div(x, y):
-    return x // y
-
-
-def sub(x, y):
-    return x - y
-
-
-print(add(1, 2, 4))
-print(sub(75, 90))
-print(mul(1, 24, 2))
+print(add(1,2,4))
+print(sub(75,90))
