b868a5fe1108141e9270a65b396969d1fa6ac5a6
Anbarasan <anbarasan.mailme@gmail.com>
2019-08-25 20:59:17 +0530

    Multiplication fucntion fixed.

--- calc.py
+++ calc.py
@@ -1,12 +1,23 @@
-def add(*args):
-	"""Adding list of values"""
-	return sum(args)
-def mul(x,y):
-	return x*y
-def div(x,y):
-	return x//y
-def sub(x,y):
-	return x-y
+import functools as ft
 
-print(add(1,2,4))
-print(sub(75,90))
+
+def add(*args):
+    """Adding list of values"""
+    return sum(args)
+
+
+def mul(*args):
+    return ft.reduce(lambda a, b: a*b, args)
+
+
+def div(x, y):
+    return x // y
+
+
+def sub(x, y):
+    return x - y
+
+
+print(add(1, 2, 4))
+print(sub(75, 90))
+print(mul(1, 2, 4))
